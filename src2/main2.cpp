2d3ce869f997b80e51c9700d480a94c4f8b79e76
John Doe <test@test.com>
2025-09-03 12:07:25 +0800

    Break down CMakeLists.txt with add_subdirectory()

--- src2/main2.cpp
+++ src2/main2.cpp
@@ -5,7 +5,7 @@ import math2;
 #include <iostream>
 
 int main() {
-  std::cout << "main 2"  << std::endl;
+  std::cout << "main 2 lulu"  << std::endl;
   std::cout << "Addition 2 + 2 = " << add(2, 2) << std::endl;
   std::cout << "Multiply 3 * 3 = " << multiply(3, 3) << std::endl;
   return 0;
